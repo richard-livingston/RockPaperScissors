406c98acc71fb4aeff2fe51c56346ee77da07889
Richard Livingston <r1ch4rdl@hotmail.com>
2014-11-16 11:30:30 +0000

    Tidied animation code on GameRevealView

--- src/GameRevealView.js
+++ src/GameRevealView.js
@@ -20,25 +20,13 @@ var rps = rps || {};
 
         var finishedPlaying = false;
 
-        var computersHand,
-            playersHand;
-
         var restartText = new createjs.Bitmap(rps.assets.getResult('text/restart')),
             winText = new createjs.Bitmap(rps.assets.getResult('text/win')),
             loseText = new createjs.Bitmap(rps.assets.getResult('text/lose')),
             drawText = new createjs.Bitmap(rps.assets.getResult('text/draw'));
 
-        this.addEventListener('addedToStage', function onAddedToStage(event){
-            finishedPlaying = false;
-
-            displayHands(game.model.computersMove, game.model.playersMove);
-            displayWinMessageText();
-            displayRestartText();
-        });
-
-        this.addEventListener('removedFromStage', function onRemovedFromStage(event){
-            self.removeAllChildren();
-        });
+        this.addEventListener('addedToStage', playAnimation);
+        this.addEventListener('removedFromStage', this.removeAllChildren.bind(this));
 
         // Start a new round on click anywhere
         this.addEventListener('click', function onClick(event){
@@ -52,19 +40,29 @@ var rps = rps || {};
         this.hitArea.graphics.beginFill('#000000');
         this.hitArea.graphics.drawRect(this.x, this.y, game.stage.canvas.width, game.stage.canvas.height);
 
-        function displayHands(computer, player){
-            computersHand = new rps.HandView(game.model.computersMove, 'right');
-            playersHand = new rps.HandView(game.model.playersMove, 'left');
+        function playAnimation(){
+            finishedPlaying = false;
 
-            computersHand.x = -computersHand.getBounds().width;
-            playersHand.x = game.stage.canvas.width;
-            computersHand.y = playersHand.y = 50;
+            displayHand(new rps.HandView(game.model.computersMove, 'right')).setPaused(false);
+            displayHand(new rps.HandView(game.model.playersMove, 'left')).setPaused(false)
+                .play(displayWinMessageText()
+                    .play(displayRestartText()
+                        .call(function onDisplayRestartTextFinished(){
+                            finishedPlaying = true;
+                        })));
+        }
 
-            createjs.Tween.get(computersHand).to({x : 0}, 400);
-            createjs.Tween.get(playersHand).to({x : playersHand.x - playersHand.getBounds().width}, 400);
+        function displayHand(hand){
+            var dir = hand.direction;
 
-            self.addChild(computersHand);
-            self.addChild(playersHand);
+            hand.x = dir == 'right' ? - hand.getBounds().width : game.stage.canvas.width;
+            hand.y = 50;
+
+            self.addChild(hand);
+
+            return createjs.Tween.get(hand, {paused : true}).to({
+                x : dir == 'right' ? 0 : hand.x - hand.getBounds().width
+            }, 400);
         }
 
         function displayWinMessageText(){
@@ -88,33 +86,29 @@ var rps = rps || {};
             image.x = game.stage.canvas.width / 2;
             image.y = game.stage.canvas.height / 2;
 
-            createjs.Tween.get(image)
-                .wait(600)
+            self.addChild(image);
+
+            return createjs.Tween.get(image, {paused : true})
+                .wait(400)
                 .to({
                     scaleX : 1,
                     scaleY : 1,
                     x : image.x - image.image.width / 2,
                     y : image.y - image.image.height / 2
                 }, 500);
-
-            self.addChild(image);
         }
 
         function displayRestartText(){
             restartText.x = (game.stage.canvas.width - restartText.image.width) / 2;
             restartText.y = - restartText.image.height;
 
-            createjs.Tween.get(restartText)
-                .wait(1500)
+            self.addChild(restartText);
+
+            return createjs.Tween.get(restartText, {paused : true})
+                .wait(200)
                 .to({
                     y : game.stage.canvas.height - restartText.image.height
-                }, 1000, createjs.Ease.bounceOut)
-
-                .call(function onDisplayRestartTextFinished(){
-                    finishedPlaying = true;
-                });
-
-            self.addChild(restartText);
+                }, 1000, createjs.Ease.bounceOut);
         }
     }
 
